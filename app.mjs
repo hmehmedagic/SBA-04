485de3ae3485ab9c3a3dead8b882070570af6f62
Hasan Mehmedagic <hmehmedagic@verizon.net>
2024-04-05 18:51:58 -0400

    updated to use MoogleAPI

--- app.mjs
+++ app.mjs
@@ -1,59 +1,36 @@
 // Main File for Classes and Functions and vars
 let body = document.querySelector(".dashboard");
 
-export default class Employee {
-    constructor(name, email, phone) {
+export default class Character {
+    constructor(name, job) {
         this.name = name;
-        this.email = email;
-        this.phone = phone;
+        this.job = job;
     }
 }
 // ----------------------------------------[CLASSES]
 export const getName = (user) => {
-    console.log(`Good Evening Welcome to TechBiz. My name is ${user.name}`);
+    console.log(`Good Evening Welcome. My name is ${user.name}`);
 }
-export const getEmail = (user) => {
-    console.log(`At TechBiz my Official email is ${user.name} ${user.email} `);
+export const getJob = (user) => {
+    console.log(`My role is ${user.job} `);
 }
-
-export const getPhone = (user) => {
-    console.log(`I can be Contacted at ${user.phone}`);
-}
-
 
 export const postUser = (user) => {
-    let empContainer = document.createElement('div')
-    empContainer.setAttribute('class', 'container')
-        // -------parentContainer
-    let nameHolder = document.createElement('div')
-    nameHolder.setAttribute('class', "namer")
-    nameHolder.innerHTML = user.name
-        // email
-    let emailHolder = document.createElement('div')
-    emailHolder.setAttribute('class', "emailer")
-    emailHolder.innerHTML = user.email
-        // phone
-    let phoneHolder = document.createElement('div')
-    phoneHolder.setAttribute('class', "caller")
-    phoneHolder.innerHTML = user.phone
+        let chrContainer = document.createElement('div')
+        chrContainer.setAttribute('class', 'container')
+            // -------parentContainer
+        let nameHolder = document.createElement('div')
+        nameHolder.setAttribute('class', "namer")
+        nameHolder.innerHTML = user.name
+            // job
+        let jobHolder = document.createElement('div')
+        jobHolder.setAttribute('class', "jober")
+        jobHolder.innerHTML = user.job
 
-    empContainer.append(nameHolder)
-    empContainer.append(emailHolder)
-    empContainer.append(phoneHolder)
-    body.append(empContainer)
-    console.log(user)
+        chrContainer.append(nameHolder)
+        chrContainer.append(jobHolder)
+        body.append(chrContainer)
+        console.log(user)
 
-}
-
-export const fetchMe = async() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((json) => {
-                json.forEach((element) => {
-                    let user = new Employee(element.name, element.website, element.phone);
-                    console.log(user)
-                    postUser(user)
-                });
-            });
     }
     // ----------------------------------------[FUNCTIONS]
--- data.mjs
+++ data.mjs
@@ -1,27 +1,161 @@
-// Instance Files for new Instances of classes and funcCall()
+import Character, { postUser } from "./app.mjs";
 
-import Employee, { getName, getEmail, getPhone, fetchMe } from "./app.mjs";
-import { getPhotos, grabSprints, getCats, getDogs } from "./helper.mjs";
-
-let testEmployee = new Employee("Chester Tester", "@biz.gov", "000-000-0000");
-
-console.log(getName(testEmployee));
-console.log(getPhone(testEmployee));
-console.log(getEmail(testEmployee));
-// ----------------------------------------[INSTANCES]
-fetchMe();
-grabSprints()
-getPhotos()
-
-// ----------------------------------------[Initialized_Funcs()]
-const selector = () => {
-    const myBtn = document.querySelector('.click-me');
-    myBtn.addEventListener('click', () => {
-        const userInput = window.prompt('Cat or Dog?').toLocaleLowerCase();
-        if (userInput === 'dog') getDogs();
-        else if (userInput === 'cat') getCats();
-        else window.alert('Please select dog or cat.');
-    });
-
+const homeScreen = () => {
+    const dash = document.querySelector('.characterContainer');
+    dash.style.display = 'none';
+    const header = document.querySelector('.moogleAPI');
+    header.style.display = 'block';
 }
-selector();
+
+const fetchMe = async(id) => {
+    const charCont = document.querySelector('.characterContainer');
+    charCont.style.display = 'block';
+    const dash = document.querySelector('.dashboard');
+    dash.style.display = 'block';
+    dash.innerHTML = '';
+    const header = document.querySelector('.moogleAPI');
+    header.style.display = 'none';
+    await fetch("https://www.moogleapi.com/api/v1/characters")
+        .then((response) => response.json())
+        .then((json) => {
+            const chars = Object.values(json).filter(value => value.origin === id);
+            chars.forEach(char => {
+                let user = new Character(char.name, char.job);
+                console.log(user);
+                postUser(user);
+            });
+        });
+}
+const buttons = document.querySelectorAll('.dropdown-content a');
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        fetchMe(event.target.id);
+    });
+});
+
+const homeBtn = document.querySelector('#mainbtn');
+homeBtn.addEventListener('click', (event) => {
+    homeScreen();
+});
+
+// Particles
+particlesJS('particles-js',
+
+    {
+        "particles": {
+            "number": {
+                "value": 30,
+                "density": {
+                    "enable": true,
+                    "value_area": 169.717
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 1,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 2,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 5,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 10,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "config_demo": {
+            "hide_card": false,
+            "background_color": "#b61924",
+            "background_image": "",
+            "background_position": "50% 50%",
+            "background_repeat": "no-repeat",
+            "background_size": "cover"
+        }
+    }
+
+);
